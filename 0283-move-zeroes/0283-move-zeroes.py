17972b03962e29b034c6186143339d0b018796f9
Meghanath <meghanathmegha0143@gmail.com>
2026-08-26 18:42:07 +0530

    Time: 2 ms (85.54%), Space: 20.6 MB (24.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,15 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         
         """
-        index=0
-        for i in range(len(nums)):
-            if(nums[i]!=0):
-                nums[index]=nums[i]
-                index+=1
-        print(nums)
-        while(index<len(nums)):
-            nums[index]=0
-            index+=1
+        i=0
+        j=0
+        while(j<len(nums)):
+            if(nums[j]!=0):
+                nums[i],nums[j]=nums[j],nums[i]
+                i=i+1
+                j=j+1
+            else:
+                j+=1
                 
 
 
